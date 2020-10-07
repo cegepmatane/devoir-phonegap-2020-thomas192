23cf41837c3bad1ae3bbbf89a8d0b567f7c5e2e0
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-10-07 22:16:50 +0200

    Affichage de VueAjouterGalaxie

--- Univers/Application.js
+++ Univers/Application.js
@@ -1,14 +1,36 @@
 class Application {
-  constructor(window, galaxieDAO, vueListeGalaxie) {
+  constructor(window, galaxieDAO, vueListeGalaxie, vueAjouterGalaxie) {
     this.window = window;
     this.galaxieDAO = galaxieDAO;
-
     this.vueListeGalaxie = vueListeGalaxie;
+    this.vueAjouterGalaxie = vueAjouterGalaxie;
 
-    this.vueListeGalaxie.initialiserListeGalaxie(this.galaxieDAO.lister());
-    this.vueListeGalaxie.afficher();
+    // Equivalent de function(galaxie){this.actionAjouterGalaxie(galaxie)}
+    this.vueAjouterGalaxie.initialiserActionAjouterGalaxie(galaxie =>
+    this.actionAjouterGalaxie(galaxie));
+
+    // Equivalent de function(){this.naviguer()}
+    this.window.addEventListener("hashchange", () => this.naviguer());
+
+    this.naviguer();
+  }
+
+  naviguer() {
+    let hash = window.location.hash;
+
+    if (!hash) {
+      this.vueListeGalaxie.initialiserListeGalaxie(this.galaxieDAO.lister());
+      this.vueListeGalaxie.afficher();
+    } else if (hash.match(/^#ajouter-galaxie/)) {
+      this.vueAjouterGalaxie.afficher();
+    }
+  }
+
+  actionAjouterGalaxie(galaxie) {
+    this.galaxieDAO.ajouter(galaxie);
+    this.window.location.hash = "#";
   }
 
 }
 
-new Application(window, new GalaxieDAO(), new VueListeGalaxie());
+new Application(window, new GalaxieDAO(), new VueListeGalaxie(), new VueAjouterGalaxie());
