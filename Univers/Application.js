f3e492ae2714493b76d8a245eecfb2fe99a6fe1b
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-10-07 17:07:11 +0200

    Affichage de la liste

--- Univers/Application.js
+++ Univers/Application.js
@@ -1,10 +1,14 @@
 class Application {
-  constructor(window, vueListeGalaxie) {
+  constructor(window, galaxieDAO, vueListeGalaxie) {
     this.window = window;
+    this.galaxieDAO = galaxieDAO;
+
     this.vueListeGalaxie = vueListeGalaxie;
+
+    this.vueListeGalaxie.initialiserListeGalaxie(this.galaxieDAO.lister());
     this.vueListeGalaxie.afficher();
   }
 
 }
 
-new Application(window, new VueListeGalaxie());
+new Application(window, new GalaxieDAO(), new VueListeGalaxie());
