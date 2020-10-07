3dc5dd088244120d19989852920f9364e5fa9475
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-10-07 16:24:26 +0200

    Ajout VueListeGalaxie

---
+++ Univers/vue/VueListeGalaxie.js
@@ -0,0 +1,27 @@
+class VueListeGalaxie {
+  constructor() {
+    this.html = document.getElementById("html-vue-liste-galaxie").innerHTML;
+    this.listeGalaxieDonnee = null;
+  }
+
+  afficher() {
+    document.getElementsByTagName("body")[0].innerHTML = this.html;
+
+    let listeGalaxie = document.getElementById("liste-galaxie");
+    const listeGalaxieItemHtml = listeGalaxie.innerHTML;
+    let listeGalaxieHtmlRemplacement = "";
+
+    for (var numeroGalaxie in this.listeGalaxieDonnee) {
+      let listeGalaxieItemHtmlRemplacement = listeGalaxieItemHtml;
+      listeGalaxieItemHtmlRemplacement =
+      listeGalaxieItemHtmlRemplacement.replace("{Galaxie.id}",
+      this.listeGalaxieDonnee[numeroGalaxie].id);
+      listeGalaxieItemHtmlRemplacement =
+      listeGalaxieItemHtmlRemplacement.replace("{Galaxie.nom}",
+      this.listeGalaxieDonnee[numeroGalaxie].nom);
+      listeGalaxieHtmlRemplacement += listeGalaxieItemHtmlRemplacement;
+    }
+
+    listeGalaxie.innerHTML = listeGalaxieHtmlRemplacement;
+  }
+}
